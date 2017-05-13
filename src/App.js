6a80d3f464daa697dd2beb74cd6516100a94d71c
Naoki Nagazumi <nk.ngzm@gmail.com>
2017-05-13 20:14:31 +0900

    WIP 途中まで作成

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
 class HamccoImage extends Component {
+  getInfoByFeel() {
+    let dir = '/img/';
+    let info = { name: '天使ハム', image: dir + 'kiki_ham.gif' };
+
+    switch(this.props.feel) {
+    case 1:   // Very BAD
+      info = { name: 'おこりハム', image: dir + 'okori_ham.gif' };
+      break;
+    case 2:   // BAD
+      info = { name: 'オニハム', image: dir + 'oni_ham.gif' };
+      break;
+    case 3:   // Normal
+      info = { name: 'キキハム', image: dir + 'kiki_ham.gif' };
+      break;
+    case 4:   // Good
+      info = { name: 'コックハム', image: dir + 'cook_ham.gif' };
+      break;
+    case 5:   // Very Good
+      info = { name: '天使ハム', image: dir + 'tensi_ham.gif' };
+      break;
+    default:
+      break;
+    }
+    return info;
+  }
   render() {
+    let info = this.getInfoByFeel();
     return (
       <div className={`hamcco-cell ${this.props.flex}`}>
-        <h4>はむっこイメージ</h4>
-        feel: {this.props.feel} / {`hamcco-cell ${this.props.flex}`}
+        <h4>{info.name} </h4>
+        <img src={info.image} alt={info.name} />
       </div>
     );
   }
@@ -15,8 +42,9 @@ class HamccoMessage extends Component {
   render() {
     return (
       <div className={`hamcco-cell ${this.props.flex}`}>
-        <h4>はむっこメッセージ</h4>
-        message: {this.props.message}
+        <div className="ham-message">
+          {this.props.message}
+        </div>
       </div>
     );
   }
@@ -25,44 +53,72 @@ class HamccoLevel extends Component {
   render() {
     return (
       <div className="hamcco-cell">
-        Level: {this.props.level}
+        ごきげんメータ
+        {this.props.level} %
+        <meter min="0" max="5" low="2" high="3" optimum="4" value={this.props.level}>
+        {this.props.level} %
+        </meter>
+      </div>
+    );
+  }
+}
+class MyTalk extends Component {
+  handleSubmit() {
+    let myTalk = ReactDOM.findDOMNode(this.refs.mytalk).value.trim();
+    if (!myTalk) {
+      return;
+    }
+    this.props.onSubmit(myTalk);
+    ReactDOM.findDOMNode(this.refs.mytalk).value = '';
+  }
+  render() {
+    return (
+      <div className="hamcco-cell">
+      {/* <input type="text" ref="mytalk" onKeyPress={() => this.handleSubmit()} />
+      */}
+        <input type="text" ref="mytalk" />
+        <button type="button" onClick={() => this.handleSubmit()} >
+          そうしん
+        </button>
       </div>
     );
   }
 }
 
-
-
 class HamApp extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      feel : 5,
-      count : 0
+      feel: 5,
+      message: 'こんにちは、なにか話しかけてね！',
+      count: 0
     };
   }
   render() {
     return (
       <main className="hamcco-container">
         <div className="hamcco-grid">
-          <HamccoImage feel={this.state.feel} flex="flex7" />
-          <HamccoMessage message="あああああ" flex="flex5"/>
+          <HamccoImage feel={this.state.feel} flex="flex6" />
+          <HamccoMessage message={this.state.message} flex="flex6"/>
         </div>
         <div className="hamcco-grid">
           <HamccoLevel level={this.state.feel} />
         </div>
-
-      {/* <div className="Hamcco">
-        <HamccoImage feel={this.state.feel} />
-        <HamccoMessage message="あああああ" />
-        <HamccoLevel feel={this.state.feel} />
-      </div>
-      <div className="HamChat">
-        <HamccoChat onSubmit={() => this.chatHamcco()} />
-      </div> */}
-
+        <div className="hamcco-grid">
+          <MyTalk onSubmit={(m) => this.sendMessage(m)} />
+        </div>
       </main>
     );
+  }
+  sendMessage(message) {
+    console.log('say message: ' + message);
+    this.setState((prevState, props) => {
+      return { 
+        count: prevState.count + 1,
+        feel: (prevState.feel % 5) + 1,
+        message : message
+      };
+    });
   }
 }
 
